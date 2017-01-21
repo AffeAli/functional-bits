676eb382570956bbdac9b8c2cce2b09543bfcaa1
Unknown <affe-ali@web.de>
2017-01-21 18:05:32 +0100

    content saving

--- src/main/java/mod/functionalbits/tileentity/SimpleFunctionalTileEntity.java
+++ src/main/java/mod/functionalbits/tileentity/SimpleFunctionalTileEntity.java
@@ -2,6 +2,8 @@ package mod.functionalbits.tileentity;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -17,11 +19,39 @@ public class SimpleFunctionalTileEntity extends TileEntity {
 	
 	public void setStructure(ItemStack stack) {
 		item = stack;
+		markDirty();
 	}
 
 	public boolean rightClickAction(EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
 		BlockPos newPos = pos.add(0, -1, 0);
 		return getWorld().getBlockState(newPos).getBlock().onBlockActivated(getWorld(), newPos, getWorld().getBlockState(newPos), playerIn, hand, heldItem, side, hitX, hitY, hitZ);
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		NBTTagCompound itemStack = (NBTTagCompound) compound.getTag("structure");
+		if(itemStack != null) item = ItemStack.loadItemStackFromNBT(itemStack);
+	}
+	
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		if(item != null) {
+			NBTTagCompound itemTag = item.writeToNBT(new NBTTagCompound());
+			compound.setTag("structure", itemTag);
+		}
+		return compound;
+	}
+	
+	@Override
+	public SPacketUpdateTileEntity getUpdatePacket() {
+		return new SPacketUpdateTileEntity(getPos(), 1, getUpdateTag());
+	}
+	
+	@Override
+	public NBTTagCompound getUpdateTag() {
+		return this.writeToNBT(new NBTTagCompound());
 	}
 	
 
