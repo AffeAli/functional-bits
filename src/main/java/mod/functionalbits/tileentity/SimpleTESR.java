cac030659b5efaeeed8f31221851d846d4d18e63
Unknown <affe-ali@web.de>
2017-01-21 14:59:28 +0100

    Fixed Rendering Gliches

--- src/main/java/mod/functionalbits/tileentity/SimpleTESR.java
+++ src/main/java/mod/functionalbits/tileentity/SimpleTESR.java
@@ -9,11 +9,12 @@ public class SimpleTESR extends TileEntitySpecialRenderer<SimpleFunctionalTileEn
 	
 	@Override
 	public void renderTileEntityAt(SimpleFunctionalTileEntity te, double x, double y, double z, float partialTicks, int destroyStage) {
+		GlStateManager.pushMatrix();
 		GlStateManager.translate(x, y, z);
 		GlStateManager.scale(2, 2, 2);
 		GlStateManager.translate(.25, .25, .25);
 		Minecraft.getMinecraft().getRenderItem().renderItem(te.getAsItemStack(), ItemCameraTransforms.TransformType.NONE);
-		
+		GlStateManager.popMatrix();
 	}
 
 }
